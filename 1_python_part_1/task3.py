9b814aa37bb36fe43bbd50b1cd0a618895ce6f54
Octavio Fernandez <octaviofernandez53@gmail.com>
2024-02-01 09:55:07 -0600

    Second commit, I finished the task 3

--- 1_python_part_1/task3.py
+++ 1_python_part_1/task3.py
@@ -14,6 +14,31 @@ Examples:
 """
 from typing import Iterable
 
-
+# word_number represents the word index we are interested in
 def build_from_unique_words(*lines: Iterable[str], word_number: int) -> str:
-    ...
+    if word_number < 0:
+        return ' '
+    
+    result = []         # result, is a list thta stores words from 'word_number' from each line
+    word_lists = []     # word_lists, a list to hold lists each containing unique words from the corresponding line
+    
+    # line.split() method is to get a list of words
+    # .append method, is for adding the unique words founded
+    for line in lines:
+        # Check if the line is not empty
+        if line.strip():
+            # Split the line into words, remove duplicates and preserve order
+            unique_words = list(set(line.split()))      # Remove duplicates and preserve order
+            word_lists.append(unique_words)
+        
+    for unique_words in word_lists:
+        if word_number - 1 < len(unique_words):
+            result.append(unique_words[word_number - 1])
+    
+    # .join method, is used to join collected words into a single string
+    return' '.join(result)
+
+print(build_from_unique_words('a b c', '1 1 1 2 3', 'cat dog milk', word_number=0))
+print(build_from_unique_words('a b c', '', 'cat dog milk', word_number=1))
+print(build_from_unique_words('1 2', '1 2 3', word_number=10))
+print(build_from_unique_words(word_number=10))   
